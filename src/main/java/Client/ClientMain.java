b63f68b818088369a8c33583c54dd8ef6bf9ce68
HalehQadyani <125292407+HalehQadyani@users.noreply.github.com>
2023-06-02 23:11:07 +0430

    commit

--- src/main/java/Client/ClientMain.java
+++ src/main/java/Client/ClientMain.java
@@ -1,7 +1,65 @@
 package Client;
 
-public class ClientMain {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class ClientMain {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
+        //get the localhost IP address, if server is running on some other IP, you need to use that
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+
+        while(true){
+            System.out.println("Enter your choice from the following menu:");
+            System.out.println("1.login 2.create account 3.Exit");
+            int choice;
+            choice = sc.nextInt();
+            //establish socket connection to server
+            socket = new Socket(host.getHostName(), 9876);
+            //write to socket using ObjectOutputStream
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("Sending request to Socket Server");
+            if(choice == 3){
+                oos.writeObject("exit");
+            }
+            else if(choice == 2) {
+                oos.writeObject("create account");
+                System.out.println("Enter username: ");
+                String username = sc.next();
+                System.out.println("Enter password: ");
+                String password= sc.next();
+                System.out.println("Birth date: ");
+                int birthDate = sc.nextInt();
+                oos.writeObject(username +","+ password +","+ birthDate);
+                //read the server response message
+                ois = new ObjectInputStream(socket.getInputStream());
+                String message = (String) ois.readObject();
+                System.out.println("Message: " + message);
+            }
+            else{
+                oos.writeObject("login");
+            }
+
+            //close resources
+            ois.close();
+            oos.close();
+            Thread.sleep(100);
+        }
+    }
+
+    public static void menu(int choice){
+        if(choice == 1){
+            System.out.println("");
+        }
     }
 }
+
--- src/main/java/Server/ServerMain.java
+++ src/main/java/Server/ServerMain.java
@@ -1,7 +1,72 @@
 package Server;
 
-public class ServerMain {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ServerMain {
+
+    //static ServerSocket variable
+    private static ServerSocket server;
+    //socket server port on which it will listen
+    private static int port = 9876;
+
+    //url, user, pass for mysql
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mysqlTest";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "halehgh1383";
+
+    public static void main(String args[]) throws SQLException, IOException, ClassNotFoundException{
+        // Create database connection
+        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        System.out.println("connected to database!");
+
+        //create the socket server object
+        server = new ServerSocket(port);
+        //keep listens indefinitely until receives 'exit' call or program terminates
+        while(true){
+            System.out.println("Waiting for the client request");
+            //creating socket and waiting for client connection
+            Socket socket = server.accept();
+            //read from socket to ObjectInputStream object
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            //convert ObjectInputStream object to String
+            String message = (String) ois.readObject();
+            System.out.println("Message Received: " + message);
+            //create ObjectOutputStream object
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+
+            if(message.equalsIgnoreCase("login")) {
+
+            }
+            else if(message.equalsIgnoreCase("create account")) {
+                String accountData = (String) ois.readObject();
+                String[] aData = accountData.split(",");
+                System.out.println(aData);
+
+                //write object to Socket
+                oos.writeObject("Hi Client, you have created an account!");
+
+            }
+            //terminate the server if client sends exit request
+            else if(message.equalsIgnoreCase("exit")) {
+                break;
+            }
+            //close resources
+            ois.close();
+            oos.close();
+            socket.close();
+        }
+        System.out.println("Shutting down Socket server!!");
+        //close the ServerSocket object
+        server.close();
     }
+
 }
